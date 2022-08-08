55c8892754192bfb09e3d93d0481b6b9b478508a
somni <me@somni.one>
2022-08-08 20:32:30 +0900

    FE: Add login state-related store item and interface TODO: more items to be added in `IUserInfo`

--- frontend/src/plugins/store/index.ts
+++ frontend/src/plugins/store/index.ts
@@ -1,8 +1,31 @@
 import { createStore } from "vuex";
+import IUserInfo from "@/interfaces/IUserInfo";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+interface StoreState {
+  loginInfo: {
+    isLoggedIn: boolean,
+    user: IUserInfo | null,
+  },
+}
+
+export default createStore<StoreState>({
+  state: {
+    loginInfo: {
+      isLoggedIn: false,
+      user: null,
+    },
+  },
+  getters: {
+    isUserLoggedIn(state): boolean {
+      return state.loginInfo.isLoggedIn;
+    },
+  },
+  mutations: {
+    registerLoginState(state, payload: IUserInfo | null): void {
+      // payload의 데이터가 IUserInfo의 형태로 존재할 경우 로그인 상태를 설정하는 것으로 처리
+      // payload가 null일 경우 로그인 상태를 지우는 것으로 처리
+      state.loginInfo.isLoggedIn = !!payload;
+      state.loginInfo.user = payload ?? null;
+    },
+  },
 });
