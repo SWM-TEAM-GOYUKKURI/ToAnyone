7499dbd5f112753b970b945d9ffbfe225c8a6c5a
somni <me@somni.one>
2022-08-08 22:22:41 +0900

    FE: turn login state checking condition into a function

--- frontend/src/components/RouteLoginStateGuard.vue
+++ frontend/src/components/RouteLoginStateGuard.vue
@@ -9,12 +9,16 @@ export default class RouteLoginStateGuard extends Vue {
     this.$router.beforeResolve((to) => {
       const jwtCookie = this.$cookies.get("userSession");
 
+      const isLoginStateReliable = () => {
+        return this.$store.getters.isUserLoggedIn &&
+          this.$store.state.loginInfo.user &&
+          jwtCookie;
+      };
+
       if(to.name !== "login") {
         // 로그인 페이지 외의 라우트로 이동하고 있는 경우...
 
-        if(!this.$store.getters.isUserLoggedIn ||
-          !this.$store.state.loginInfo.user ||
-          !jwtCookie) {
+        if(!isLoginStateReliable()) {
           // 로그인 상태 정보(store state + JWT cookie)가 비정상적일 때...
 
           // if(this.$route.path !== "/") {     <- 현재 루트가 앱 메인 페이지로 사용 중이기 때문에 강제 이동 예외 라우트에서 제외
@@ -28,9 +32,7 @@ export default class RouteLoginStateGuard extends Vue {
       } else {
         // 로그인 페이지로 이동하고 있는 경우...
 
-        if(this.$store.getters.isUserLoggedIn &&
-          this.$store.state.loginInfo.user &&
-          jwtCookie) {
+        if(isLoginStateReliable()) {
           // 로그인 상태 정보가 정상적일 때...
 
           // 이미 로그인 상태라고 판단하여 메인 페이지로 강제 이동
