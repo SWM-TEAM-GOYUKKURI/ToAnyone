a0a264b4f6ba7c667c4863e8bf1db962a8157368
somni <me@somni.one>
2022-08-10 20:18:47 +0900

    FE: Add some notes for login process

--- frontend/src/components/signin/SignInWithGoogle.vue
+++ frontend/src/components/signin/SignInWithGoogle.vue
@@ -29,6 +29,9 @@ export default class SignInWithGoogle extends Vue {
   }
 
   async loginCallback(data: GoogleAuthResponse) {
+    // NOTE: JWT 토큰 파싱은 백엔드에서 처리하고, 파싱된 데이터(ID, 계정 이름 등)는 로그인 처리 응답의 일부로 받도록 합니다.
+    //       프론트엔드에서는 백엔드에 이 JWT 토큰을 그대로 전송합니다.
+
     // to be filled
   }
 }
