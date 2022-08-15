fe9c077dc273cb5e7f0bf41f2c2268001c778030
somni <me@somni.one>
2022-08-15 23:07:03 +0900

    FE: REALLY BASIC styling for Login page

--- frontend/src/pages/app/signin/LoginPage.vue
+++ frontend/src/pages/app/signin/LoginPage.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <span>{{ loadState }}</span>
-    <sign-in-with-google :loaded="googleLoadState" />
-    <sign-in-with-kakao />
+  <div id="login-page">
+    <div class="logo-area">LOGO<br>HERE</div>
+
+    <div class="login-button-area">
+      <sign-in-with-google :loaded="googleLoadState" />
+      <sign-in-with-kakao />
+    </div>
   </div>
 </template>
 
@@ -32,3 +35,44 @@ export default class LoginPage extends Vue {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+#login-page {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+
+  .logo-area {
+    font-size: 5em;
+    font-weight: 700;
+    text-align: center;
+  }
+
+  .login-button-area {
+    display: flex;
+    flex-direction: column;
+
+    .button-login {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      width: 400px;
+      max-width: 100vw;
+      height: 40px;
+      padding: 0.5em;
+      margin: 0.5em;
+
+      cursor: pointer;
+
+      &:not(.no-styling) {
+        background-color: rgba(0, 0, 0, 0.25);
+      }
+    }
+  }
+}
+</style>
