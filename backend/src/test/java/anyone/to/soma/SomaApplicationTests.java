a4225ba0bf1a2cfdb4cffe82a78512bda2e03c90
grace_goose <choieungi@gm.gist.ac.kr>
2022-08-15 20:42:09 +0900

    feat: backend load

---
+++ backend/src/test/java/anyone/to/soma/SomaApplicationTests.java
@@ -0,0 +1,13 @@
+package anyone.to.soma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
