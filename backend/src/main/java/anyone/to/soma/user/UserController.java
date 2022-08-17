ad274ac9958c2e02340ca8365e2525854078748a
grace_goose <choieungi@gm.gist.ac.kr>
2022-08-17 11:54:46 +0900

    fix: get -> post

--- backend/src/main/java/anyone/to/soma/user/UserController.java
+++ backend/src/main/java/anyone/to/soma/user/UserController.java
@@ -3,6 +3,7 @@ package anyone.to.soma.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +14,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/login/google")
+    @PostMapping("/login/google")
     public ResponseEntity<String> doGoogleLogin(HttpServletRequest request) {
         String token = request.getHeader("credential");
         if (token.isBlank()) {
