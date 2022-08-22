5b739ae29217dd8002004aa92f7f6802672b20d3
somni <me@somni.one>
2022-08-22 23:53:13 +0900

    FE: Accept `withCredentials` in backend utility functions

--- frontend/src/util/backend.ts
+++ frontend/src/util/backend.ts
@@ -2,10 +2,10 @@ import axios, { AxiosRequestHeaders } from "axios";
 
 const BE_URL: string = process.env.VUE_APP_BACKEND_URL;
 
-export async function beGET(endpoint: string, headers?: AxiosRequestHeaders) {
-  return await (await axios.get(`${BE_URL}/${endpoint}`, { headers })).data as Record<string, unknown>;
+export async function beGET(endpoint: string, headers?: AxiosRequestHeaders, withCredentials?: boolean) {
+  return await (await axios.get(`${BE_URL}/${endpoint}`, { headers, withCredentials })).data as Record<string, unknown>;
 }
 
-export async function bePOST(endpoint: string, data: Record<string, unknown>, headers?: AxiosRequestHeaders) {
-  return await (await axios.post(`${BE_URL}/${endpoint}`, data, { headers })).data as Record<string, unknown>;
+export async function bePOST(endpoint: string, data: Record<string, unknown>, headers?: AxiosRequestHeaders, withCredentials?: boolean) {
+  return await (await axios.post(`${BE_URL}/${endpoint}`, data, { headers, withCredentials })).data as Record<string, unknown>;
 }
