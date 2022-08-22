693fcc9cb79e45c235278c9f91f7126871d34dbb
somni <me@somni.one>
2022-08-22 23:23:38 +0900

    FE: Accept headers in backend utility functions

--- frontend/src/util/backend.ts
+++ frontend/src/util/backend.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios, { AxiosRequestHeaders } from "axios";
 
 const BE_URL: string = process.env.VUE_APP_BACKEND_URL;
 
-export async function beGET(endpoint: string) {
-  return await (await axios.get(`${BE_URL}/${endpoint}`)).data as Record<string, unknown>;
+export async function beGET(endpoint: string, headers?: AxiosRequestHeaders) {
+  return await (await axios.get(`${BE_URL}/${endpoint}`, { headers })).data as Record<string, unknown>;
 }
 
-export async function bePOST(endpoint: string, data: Record<string, unknown>) {
-  return await (await axios.post(`${BE_URL}/${endpoint}`, data)).data as Record<string, unknown>;
+export async function bePOST(endpoint: string, data: Record<string, unknown>, headers?: AxiosRequestHeaders) {
+  return await (await axios.post(`${BE_URL}/${endpoint}`, data, { headers })).data as Record<string, unknown>;
 }
