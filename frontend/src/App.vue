d71656f255ced9d7ef1f37c819cdf58befe3a6b0
somni <me@somni.one>
2022-08-24 00:06:38 +0900

    FE: Apply fade transition for global `router-view` too

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <v-fade-transition>
+      <component :is="Component" />
+    </v-fade-transition>
+  </router-view>
 </template>
 
 <script lang="ts">
