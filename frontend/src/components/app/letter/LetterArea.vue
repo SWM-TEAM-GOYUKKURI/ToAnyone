1ebc49be8b46ef6d011248b63018addd362e4d43
somni <me@somni.one>
2022-08-25 20:50:49 +0900

    FE: Componentize letter area for further better reusability

---
+++ frontend/src/components/app/letter/LetterArea.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="letter-area">
+    <div class="letter-area__content-area">
+      <div><strong>From. {{ realSenderNickname }}</strong></div>
+      <contenteditable v-model="letterTextContent"
+                       class="letter-area__content-area__text-area"
+                       tag="div"
+                       :contenteditable="letterWriteMode"
+                       :no-html="true"
+                       @keydown.ctrl="onEditorKeyDown"></contenteditable>
+      <div style="text-align: right"><strong>To. {{ realReceiverNickname }}</strong></div>
+    </div>
+
+    <div v-if="letterWriteMode"
+         class="letter-area__send-button animation-button"
+         @click="$emit('sendButtonClick')">
+          <v-icon>mdi-send</v-icon>
+          <span>편지 보내기</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { Prop, PropSync } from "vue-property-decorator";
+import contenteditable from "vue-contenteditable";
+
+@Options({
+  components: {
+    contenteditable,
+  },
+})
+export default class LetterArea extends Vue {
+  @Prop({ type: Boolean, default: false }) letterWriteMode!: boolean;
+  @Prop({ type: String, default: "SENDER" }) senderNickname!: string;
+  @Prop({ type: String, default: "RECEIVER" }) receiverNickname!: string;
+  @PropSync("textContent", { type: String, default: "" }) letterTextContent!: string;
+
+  get realReceiverNickname(): string {
+    return this.letterWriteMode ? "Anyone" : this.receiverNickname;
+  }
+
+  get realSenderNickname(): string {
+    return this.letterWriteMode ? this.$store.state.auth.userBasicInfo!.nickname : this.senderNickname;
+  }
+
+  onEditorKeyDown(event: KeyboardEvent): void {
+    if(event.ctrlKey) {
+      switch(event.key.toLowerCase()) {
+        case "s":
+          // CTRL+S : 임시 저장
+          event.preventDefault();
+          this.$emit("requestTempSave");
+          break;
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.letter-area {
+  display: flex;
+  position: relative;
+  padding: 1em;
+  font-size: 1.5em;
+  border-radius: 1em;
+  background-color: #E9E9CC;
+
+  &__content-area {
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+    width: 100%;
+    overflow: hidden;
+    font-family: serif;
+    color: #310;
+    padding-bottom: 2em;
+
+    &__text-area {
+      flex-grow: 1;
+      width: 100%;
+      margin: 0.5em 0;
+      line-height: 2;
+
+      &:focus, &:focus-visible, &:focus-within {
+        border: none;
+        outline: none;
+      }
+    }
+  }
+
+  &__send-button {
+    cursor: pointer;
+    position: absolute;
+    right: -1.5em;
+    bottom: -1.5em;
+    padding: 1em;
+    display: flex;
+    align-items: center;
+    background-color: $color-primary;
+    border-radius: 999em;
+    box-shadow: 0 0.33em 0.5em rgba(black, 0.25);
+    z-index: 1;
+
+    & > * {
+      margin: 0 0.25em;
+    }
+  }
+}
+</style>
--- frontend/src/pages/app/letter/LetterWritePage.vue
+++ frontend/src/pages/app/letter/LetterWritePage.vue
@@ -4,33 +4,22 @@
       꾸미기 아이템 영역<br>구현 중입니다.
     </div>
 
-    <div id="letter-paper">
-      <div class="letter-paper__content-area">
-        <div><strong>From. {{ $store.state.auth.userBasicInfo.nickname }}</strong></div>
-        <contenteditable v-model="letterTextContent"
-                        class="letter-paper__content-area__text-area"
-                        tag="div"
-                        contenteditable="true"
-                        @keydown.ctrl="onEditorKeyDown"></contenteditable>
-        <div style="text-align: right"><strong>To. Anyone</strong></div>
-      </div>
-
-      <div class="letter-paper__send-button animation-button"
-           @click="onSendButtonClick">
-           <v-icon>mdi-send</v-icon>
-           <span>편지 보내기</span>
-      </div>
-    </div>
+    <letter-area id="letter-write-area"
+                 v-model:textContent="letterTextContent"
+                 :letterWriteMode="true"
+                 @sendButtonClick="onSendButtonClick" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import contenteditable from "vue-contenteditable";
+import LetterArea from "@/components/app/letter/LetterArea.vue";
 
 @Options({
   components: {
     contenteditable,
+    LetterArea,
   },
 })
 export default class LetterWritePage extends Vue {
@@ -78,59 +67,11 @@ export default class LetterWritePage extends Vue {
     padding: 1rem;
   }
 
-  #letter-paper {
-    position: relative;
+  #letter-write-area {
     width: $letter-paper-area-width;
     min-height: $contents-min-height;
     margin-right: calc((100vw - $decor-area-width - $letter-paper-area-width) / 2.5);
     margin-left: auto;
-    padding: 1em;
-    font-size: 1.5em;
-    border-radius: 1rem;
-    background-color: #E9E9CC;
-
-    .letter-paper {
-      &__content-area {
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        min-height: calc($contents-min-height - ($letter-paper-area-padding * 2));
-        overflow: hidden;
-        font-family: serif;
-        color: #310;
-        padding-bottom: 2em;
-
-        &__text-area {
-          flex-grow: 1;
-          width: 100%;
-          margin: 0.5em 0;
-          line-height: 2;
-
-          &:focus, &:focus-visible, &:focus-within {
-            border: none;
-            outline: none;
-          }
-        }
-      }
-
-      &__send-button {
-        cursor: pointer;
-        position: absolute;
-        right: -1.5em;
-        bottom: -1.5em;
-        padding: 1em;
-        display: flex;
-        align-items: center;
-        background-color: $color-primary;
-        border-radius: 999em;
-        box-shadow: 0 0.33em 0.5em rgba(black, 0.25);
-        z-index: 1;
-
-        & > * {
-          margin: 0 0.25em;
-        }
-      }
-    }
   }
 }
 </style>
