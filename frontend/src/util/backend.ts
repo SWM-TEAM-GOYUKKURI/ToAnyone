88012ee3921b11339f3f03448d280fb5b9d2e590
somni <me@somni.one>
2022-08-31 18:26:46 +0900

    FE: Add status code in returned object of backend util functions

--- frontend/src/util/backend.ts
+++ frontend/src/util/backend.ts
@@ -2,10 +2,20 @@ import axios, { AxiosRequestHeaders } from "axios";
 
 const BE_URL: string = process.env.VUE_APP_BACKEND_URL;
 
-export async function beGET(endpoint: string, headers?: AxiosRequestHeaders) {
-  return await (await axios.get(`${BE_URL}/${endpoint}`, { headers })).data as Record<string, unknown>;
+export async function beGET(endpoint: string, headers?: AxiosRequestHeaders): Promise<{ statusCode: number, data: Record<string, unknown> }> {
+  const response = await axios.get(`${BE_URL}/${endpoint}`, { headers });
+
+  return {
+    statusCode: response.status,
+    data: await response.data as Record<string, unknown>,
+  };
 }
 
-export async function bePOST(endpoint: string, data: Record<string, unknown>, headers?: AxiosRequestHeaders) {
-  return await (await axios.post(`${BE_URL}/${endpoint}`, data, { headers })).data as Record<string, unknown>;
+export async function bePOST(endpoint: string, data: Record<string, unknown>, headers?: AxiosRequestHeaders): Promise<{ statusCode: number, data: Record<string, unknown> }> {
+  const response = await axios.post(`${BE_URL}/${endpoint}`, data, { headers });
+
+  return {
+    statusCode: response.status,
+    data: await response.data as Record<string, unknown>,
+  };
 }
