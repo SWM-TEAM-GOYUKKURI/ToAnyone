a0b5a516f33a202421b088770812d919acabbf4b
somni <me@somni.one>
2022-09-15 19:42:04 +0900

    FE: Implement logout & initial logout page

--- frontend/src/pages/app/signin/LogoutPage.vue
+++ frontend/src/pages/app/signin/LogoutPage.vue
@@ -1,11 +1,16 @@
 <template>
-  <div></div>
+  <div>😥 로그아웃하고 있어요.</div>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
 
 export default class LogoutPage extends Vue {
-
+  mounted(): void {
+    // Unconditional logout
+    this.$store.commit("auth/registerLoginState", null);
+    this.$cookies.remove("userSession");
+    window.location.replace("/");
+  }
 }
 </script>
