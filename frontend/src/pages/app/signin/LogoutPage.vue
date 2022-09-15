cba7ead667391f1604c86252a2e8b8b9d0af5ba0
somni <me@somni.one>
2022-09-15 19:55:35 +0900

    FE: Simple styling for logout page

--- frontend/src/pages/app/signin/LogoutPage.vue
+++ frontend/src/pages/app/signin/LogoutPage.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>😥 로그아웃하고 있어요.</div>
+  <div>
+    <v-slide-y-reverse-transition>
+      <span v-show="pageMounted">🫠 로그아웃하고 있어요.</span>
+    </v-slide-y-reverse-transition>
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
 
 export default class LogoutPage extends Vue {
+  pageMounted = false;
+
   mounted(): void {
+    this.pageMounted = true;
+
     // Unconditional logout
     this.$store.commit("auth/registerLoginState", null);
     this.$cookies.remove("userSession");
@@ -14,3 +22,16 @@ export default class LogoutPage extends Vue {
   }
 }
 </script>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  font-size: 3em;
+  text-align: center;
+  line-height: 1.25;
+}
+</style>
