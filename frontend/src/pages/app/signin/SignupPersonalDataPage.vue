af7d210fed85f7e1ee554a91f2c52a57298b70be
somni <me@somni.one>
2022-09-29 23:08:34 +0900

    FE: Add skip signup button (ONLY IN DEV ENV)

--- frontend/src/pages/app/signin/SignupPersonalDataPage.vue
+++ frontend/src/pages/app/signin/SignupPersonalDataPage.vue
@@ -1,11 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <div v-if="devMode"
+         @click="devSkipInput">SKIP</div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
 
 export default class SignupPersonalDataPage extends Vue {
+  get devMode(): boolean {
+    return process.env.VUE_APP_DEVMODE === "true";
+  }
 
+  devSkipInput(): void {
+    if(this.devMode) {
+      this.$store.commit("auth/registerLoginState", {
+        user: {
+          ...this.$store.state.auth.userBasicInfo,
+          signedUp: true,
+        },
+        token: this.$store.state.auth.token,
+      });
+      this.$router.replace({ name: "main" });
+    }
+  }
 }
 </script>
