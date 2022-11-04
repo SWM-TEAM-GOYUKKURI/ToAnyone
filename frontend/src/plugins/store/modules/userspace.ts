33a07122198d44154a3e17134d7675243f3e55db
somni <me@somni.one>
2022-11-04 22:11:34 +0900

    FE: Add unread letters store state and update mutations

--- frontend/src/plugins/store/modules/userspace.ts
+++ frontend/src/plugins/store/modules/userspace.ts
@@ -1,9 +1,11 @@
 import { Module } from "vuex";
 import { RootStoreState } from "..";
 import { UserInfo } from "@/interfaces/internal";
+import { LetterInboxItemList } from "@/interfaces/backend";
 
 export interface UserspaceStoreState {
   user: UserInfo | null,
+  unreadLetters: LetterInboxItemList,
   settings: {
     // to be filled
   },
@@ -13,8 +15,13 @@ const userspaceStore: Module<UserspaceStoreState, RootStoreState> = {
   namespaced: true,
   state: {
     user: null,
+    unreadLetters: [],
     settings: { },
   },
+  mutations: {
+    updateUserInfo(state, payload: UserInfo) { state.user = payload; },
+    updateUnreadLetters(state, payload: LetterInboxItemList) { state.unreadLetters = payload; },
+  }
 };
 
 export default userspaceStore;
