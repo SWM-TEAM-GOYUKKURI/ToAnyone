51f8de97c1fb6c05a176d8079b83445bb13b82b1
somni <me@somni.one>
2022-11-13 00:54:36 +0900

    FE: Update point help message

--- frontend/src/pages/app/user/PointHelpView.vue
+++ frontend/src/pages/app/user/PointHelpView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <span>개발 중...</span><br><br>
-    <span>포인트는 아이템 상점에서 아이템을 구매할 때 사용할 수 있어요.</span><br>
-    <span>포인트는 로그인, 편지 쓰기, 업적 달성 등으로 얻을 수 있어요.</span>
+  <div style="line-height: 2">
+    <p>포인트는 아이템 상점에서 아이템을 구매할 때 사용할 수 있어요.</p>
+    <p>포인트는 로그인, 편지 쓰기, 업적 달성 등으로 얻을 수 있어요.</p>
+    <p>포인트는 실제 재화로 구매하거나 전환할 수 없어요.</p>
 
     <div class="point-help__controls">
       <button class="button"
