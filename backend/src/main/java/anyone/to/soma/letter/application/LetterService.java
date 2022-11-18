173266d7f472d7d96cb458f54901dad110093e15
grace_goose <choieungi@gm.gist.ac.kr>
2022-11-18 17:14:18 +0900

    fix: 보낸 답장 개수 구현

--- backend/src/main/java/anyone/to/soma/letter/application/LetterService.java
+++ backend/src/main/java/anyone/to/soma/letter/application/LetterService.java
@@ -76,6 +76,7 @@ public class LetterService {
         User replyLetterReceiver = letter.findReplyLetterReceiver(replySender);
         ReplyLetter replyLetter = new ReplyLetter(request.getContent(), LocalDate.now(), letter, replySender.getNickname(), replySender.getUserImageUrl(), replyLetterReceiver.getNickname(), replyLetterReceiver.getUserImageUrl(), request.getDecorations());
         letter.reply(replyLetter, replySender);
+        userRepository.increaseSendReplyLetterCount(replySender.getId());
     }
 
     public List<InboxLetterResponse> retrieveSentLetters(User sender) {
